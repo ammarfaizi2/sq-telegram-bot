2f78f008d21652c56d0046e2bc83a36f3e0f4e43
Ammar Faizi <ammarfaizi2@gmail.com>
2018-11-04 17:34:35 +0700

    Turn off dummy

--- public/adminer.php
+++ public/adminer.php
@@ -2,16 +2,8 @@
 
 require_once __DIR__."/../bootstrap/web_init.php";
 
-if (!(isset($_SESSION["login"]) && $_SESSION["login"] === true)) {
-	header("Location: /login.php?ref=home&w=".urlencode(rstr(64)));
-	exit;
-}
-
-unset($_SESSION["login"]);
-
 function isolateAdminer() {
 	ini_set("display_errors", false);
 	@require __DIR__."/../adminer.php";
 }
 isolateAdminer();
-$_SESSION["login"] = true;
